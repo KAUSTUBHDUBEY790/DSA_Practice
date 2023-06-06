94d9c9bbf4fcf69df59cc3282dd14244833ca43c
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2023-06-06 10:36:31 +0530

    commit

--- trees/preorder.java
+++ trees/preorder.java
@@ -22,11 +22,21 @@ public class preorder{
             head.right = buildtree(Nodes);
             return head;
         }
+        public static void printtree(Node head)
+        {
+            if(head == null)
+            return;
+            System.out.println(head.data+" ");
+            printtree(head.left);
+            printtree(head.right);
+
+        }
     }
         public static void main(String args[]) {
             int Nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
             Preodertree tree= new Preodertree();
             Node root = tree.buildtree(Nodes);
-            System.out.println(root.data);
+            // System.out.println(root.data);
+            tree.printtree(root);
         }
 }
